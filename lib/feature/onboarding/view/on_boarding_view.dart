9d72b010e24a88c210d67a6fbda97ac3c0557c1b
John Safwat <106314101+john-safwat@users.noreply.github.com>
2025-04-14 22:56:32 +0200

    modify some file

--- lib/feature/onboarding/view/on_boarding_view.dart
+++ lib/feature/onboarding/view/on_boarding_view.dart
@@ -23,8 +23,6 @@ class _OnBoardingViewState extends State<OnBoardingView> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(),
-
-
             OutlinedButton(
               onPressed: () {
                 getIt<AppConfigProvider>().changeLocale(
